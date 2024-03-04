cef708ab71b159c3e6b8b3f930639c79dd306642
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-03-04 16:16:32 +0000

    Home page is mobile responsive

--- frontend/src/components/BlogItem.js
+++ frontend/src/components/BlogItem.js
@@ -7,21 +7,22 @@ const BlogItem = ({blog}) => {
   return (
     <Link to={`/blog/${blog.id}`}>
         <div className="blog-item">
-          <div >
+          <div>
             {blog.cover ? (
               <img className='cover' src={blog.cover}  alt='blog' />
             ) : (
               <img className='cover' src={Placeholder} alt='blog' />
             )}
           </div>
-          
+
           <div>
             <h1 className='blog-title'>{blog.title}</h1>
-            <h2><span>{blog.subtitle}</span></h2>
+            <h2 className='blog-subtitle'><span>{blog.subtitle}</span></h2>
             <p className='published'>
               Published <span>{getTime(blog.created)}</span>
             </p>
           </div>
+          <hr />
         </div>
       </Link>
   )
